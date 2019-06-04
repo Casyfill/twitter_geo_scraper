494b1c3e626b9beba7ec959166991a9e7ff562dc
Philipp Kats <casyfill@gmail.com>
2019-06-03 21:02:51 -0400

    dump to s3

--- data_pipelines/data_to_s3.py
+++ data_pipelines/data_to_s3.py
@@ -1,39 +1,86 @@
 import luigi
 from luigi.contrib.s3 import S3Client, S3Target
-from os.path import dirname, join
+# from os.path import dirname, join, basename
+from pathlib import Path
 from datetime import datetime
 from glob import glob
-CREDS_PATH = join(dirname(__file__), '..', '..', 'credentials.json')
+import pandas as pd
+import psycopg2
+# CREDS_PATH = join(dirname(__file__), '..', '..', 'credentials.json')
 
 
-class send_file_to_S3(luigi.Task):
-        path = luigi.Parameter()
+class Dump_month_to_s3(luigi.Task):
 	client = S3Client()
+	month = luigi.DateIntervalParameter('2015-01')
+	v = luigi.NumericalParameter(
+        default=0.1, var_type=float, min_value=0, max_value=100
+    )
 
-        def  output(self):
-		s3_path =  self.s3_base + basename(self.path)
+	self.s3_base.format = 'https://qctwitterarchive.nyc3.digitaloceanspaces.com/postgresql_dump/{v}/{date:%Y/%m}.csv'
+	pgscon = psycopg2.connect(
+		host='localhost',
+		database='tweets',
+		user='root',
+		password='newyork04'
+	)
+		
+	Q = '''SELECT id, user_id, timestamp, lat, lon, application, ct FROM tweets
+		WHERE timestamp BETWEEN '{S:%Y-%m-%d} 00:00:00' AND '{E:%Y-%m-%d} 00:00:00';
+		'''
+
+	@property
+	def period(self):
+		return self.month.date_a.timestamp(), self.month.date_b.timestamp()
+
+	def _get_data(self):
+		Q = self.Q.format(*self.period)
+		return pd.read_sql(Q, self.pgscon)
+
+	def  output(self):
+		s3_path =  self.s3_base.format(date=self.period.date_a, v=self.v)
 		return S3Target(path=s3_path, client=self.client)
 
-        def run(self):
-		self.client.put(self.path, self.output().path,
-				headers={'Content-Type':'application/x-sqlite3'})
+	def run(self):
+		data = self._get_data()
+		print(f'uploading {len(data)} rows for {self.period}')
+		self._upload_csv(data, self.output().path)
+
+	def _upload_csv(self, df, path):
+		content = df.to_csv(float_format="%.3f", index=None)
+        self.client.put_string(
+            content=content, destination_s3_path=path, ContentType="text/csv"
+        )
 
 
-class send_all_db_to_S3(luigi.Task):
-	data_folder = os.getenv('TWITTERDATAPATH')
-	log_folder = os.getenv('TWITTERLOGS')
-	all_files = glob(data_folder + '/*.db')
 
-	def requires(self):
-		return [send_file_to_S3(path=p) for p in self.all_files]
+# class send_file_to_S3(luigi.Task):
+#     path = luigi.Parameter()
+# 	client = S3Client()
 
-	def output(self):
-		return luigi.LocalTarget(join(self.log_folder, datetime.now().strftime('%Y-%m-%d_%H_%M.log')))
+#     def  output(self):
+# 		s3_path =  self.s3_base + basename(self.path)
+# 		return S3Target(path=s3_path, client=self.client)
 
-	def run(self)
-		with self.output().open('r') as f:
-			f.write('all data moved to s3')
+#     def run(self):
+# 		self.client.put(self.path, self.output().path,
+# 						headers={'Content-Type':'application/x-sqlite3'})
 
 
-if __name__ == '__main__':
-    luigi.run(main_task_cls=send_all_db_to_S3, local_scheduler=True)
+# class send_all_db_to_S3(luigi.Task):
+# 	data_folder = os.getenv('TWITTERDATAPATH')
+# 	log_folder = os.getenv('TWITTERLOGS')
+# 	all_files = glob(data_folder + '/*.db')
+
+# 	def requires(self):
+# 		return [send_file_to_S3(path=p) for p in self.all_files]
+
+# 	def output(self):
+# 		return luigi.LocalTarget(join(self.log_folder, datetime.now().strftime('%Y-%m-%d_%H_%M.log')))
+
+# 	def run(self)
+# 		with self.output().open('r') as f:
+# 			f.write('all data moved to s3')
+
+
+# if __name__ == '__main__':
+#     luigi.run(main_task_cls=send_all_db_to_S3, local_scheduler=True)
--- data_pipelines/sqlite_to_psql.py
+++ data_pipelines/sqlite_to_psql.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from datetime import date
+from datetime import date, datetime
 from glob import glob
 from pathlib import Path
 
@@ -8,16 +8,16 @@ import luigi
 # import pandas as pd
 from luigi.contrib.postgres import CopyToTable, PostgresTarget
 
-DATA_PATH = '/root/data_dumps'
+DATA_PATH = os.getenv('TWITTERDATAPATH') #'/root/data_dumps'
 
 class SQLite_to_Postgres(CopyToTable):
 
-    sqlite_path = luigi.Parameter(var_type=str)
+    sqlite_path = luigi.Parameter(default='~/data_dumps/DO2_2018-03-12_17_20.db')
     
     host='localhost'
     database='twitter'
     user='root'
-    password=os.environ['PSQL_PASSWORD']
+    password='newyork04'
     table='tweets'
     columns= ['id', 'timestamp', 'lon', 'lat',
              'text', 'user_id',
@@ -39,17 +39,24 @@ class SQLite_to_Postgres(CopyToTable):
 
     def rows(self):
         sqc = self._sqlite_connect()
-        return sqc.execute(self.SQQ).fetchall()
+        
+        try:
+            data = sqc.execute(self.SQQ).fetchall()
+        except sqlite3.OperationalError:
+            data = []  # empty file
+
+        print(f'Writing {len(data)} rows to Postgres')
+        return data
 
 
 class BulkSQLite(luigi.Task):
 
-    mindate = luigi.DateParameter(default='2018-03-02')
+    mindate = luigi.DateHourParameter(default=datetime(2018,3,2, 0))
 
     @staticmethod
     def _filter(path, mindate):
         _name = Path(path).name.split('_')[1]
-        _date = date.strptime(_name, '%Y-%m-%d')
+        _date = datetime.strptime(_name, '%Y-%m-%d')
         return _date > mindate
 
     def _get_paths(self):
@@ -58,4 +65,5 @@ class BulkSQLite(luigi.Task):
         return [p for p in _paths if self._filter(p, self.mindate)]
         
     def requires(self):
-        return [SQLite_to_Postgres(sqlite_path=path) for path in self.paths]
+        paths = self._get_paths()
+        return [SQLite_to_Postgres(sqlite_path=path) for path in paths]
