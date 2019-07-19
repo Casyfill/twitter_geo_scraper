aa613ab3e3e6c205e2e9caf167c5a1486b26d279
Philipp Kats <casyfill@gmail.com>
2019-07-19 23:21:25 +0000

    replace print with logger

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -138,10 +138,10 @@ def main():
             try:
                 for name, df in dbs.items():
                     if len(df) > 0:
-                        print(f'Writing to {name}: {len(df)}')
+                        logger.info(f'Writing to {name}: {len(df)}')
                         df.to_sql(name, con=conn, if_exists='append', index=False)
                     else:
-                        print(f'No data in {name}')
+                        logger.info(f'No data in {name}')
                 #conn.commit()
 
                 node['since'] = t['search_metadata']['max_id_str']
