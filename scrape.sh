df2dbaff9355edd8945b40e2531ea53dde96768c
Philipp Kats <casyfill@gmail.com>
2019-10-09 14:21:45 -0400

    minor change to scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 if ps -ef | grep -v grep | grep twitter_scraper.py ; then
+        echo "seems to work fine"
         exit 0
 else
         
         python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py &
+        echo "no scraper working, rebooting!"
         exit 0
 fi
