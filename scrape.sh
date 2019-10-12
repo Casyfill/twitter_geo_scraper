cb8710dd13ee259b7535ad8c2754fe0836a9efa5
Philipp Kats <casyfill@gmail.com>
2019-10-12 16:19:04 -0400

    bbetter (?) rebooting script

--- scrape.sh
+++ scrape.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 if ps -ef | grep -v grep | grep twitter_scraper.py ; then
-        echo "seems to work fine"
-        exit 0
-else  
-        python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py &
-        echo "no scraper working, rebooting!"
-        exit 0
+    echo "rebooting twitter scraper"
+    pkill -u pbk236 -f "twitter_geo_scraper"
+    python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py
+    echo "xoxoxo"
+    exit 0
+else
+    echo "no scraper working, rebooting!" &
+    python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py &
+    exit 0
 fi
+      
