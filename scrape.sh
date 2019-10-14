fbef5003c4525521bfe9ad73966a30fa1cffb7d8
Philipp Kats <casyfill@gmail.com>
2019-10-14 12:34:34 -0400

    working simple bash script

--- scrape.sh
+++ scrape.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-if ps -ef | grep -v grep | grep twitter_scraper.py ; then
-    echo "rebooting twitter scraper"
-    pkill -u pbk236 -f "twitter_geo_scraper"
-    python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py
-    echo "xoxoxo"
-    exit 0
-else
-    echo "no scraper working, rebooting!" &
-    python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py &
-    exit 0
-fi
-      
+pkill -u pbk236 -f "twitter_geo_scraper/twitter" || echo "Scraper was not running."
+
+echo "restarting twitter scraper"
+python /home/cusp/pbk236/twitter_geo_scraper/twitter_scraper.py &
+exit 0
